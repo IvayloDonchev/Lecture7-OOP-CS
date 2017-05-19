15dbd30035a76c6304773d206d4dff20bd1ac05d
Ivaylo Donchev <idonchev@hotmail.com>
2017-05-19 17:26:27 +0300

    dfgh

--- Lecture7/Operator Overloading/Vector.cs
+++ Lecture7/Operator Overloading/Vector.cs
@@ -22,5 +22,13 @@ namespace Operator_Overloading
         public static bool operator >(Vector v1, Vector v2) => v1.Size > v2.Size;
         public static bool operator ==(Vector v1, Vector v2) => v1.Size == v2.Size;
         public static bool operator !=(Vector v1, Vector v2) => v1.Size != v2.Size;
+        public override bool Equals(object obj)
+        {
+            return this.Size == (obj as Vector).Size;
+        }
+        public override int GetHashCode()
+        {
+            return (int)(X*X + Y*Y);
+        }
     }
 }
