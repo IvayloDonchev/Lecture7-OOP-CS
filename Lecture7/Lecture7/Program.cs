b964cb238fc68ed11aa7e79c315b99cbdc31c9ca
Ivaylo Donchev <idonchev@hotmail.com>
2017-05-19 10:09:10 +0300

    rtg

---
+++ Lecture7/Lecture7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture7
+{
+    class Program
+    {
+        public Task Handl(IndexOutOfRangeException e)
+        {
+            return new Task(null);
+        }
+        public  async Task Method()
+        {
+            int[] myArray = { 1, 2, 3, 4 };
+            try
+            {
+                //..........................
+                int myElem = myArray[4];
+            }
+            catch (IndexOutOfRangeException e) when (myArray.Length > 100)
+            {
+                await Handl(e);
+            }
+            finally
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
