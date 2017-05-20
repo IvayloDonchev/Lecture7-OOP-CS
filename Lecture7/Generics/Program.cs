d915a0879cb4fa35f979d3baf3f5e3efae6bb346
Ivaylo Donchev <idonchev@hotmail.com>
2017-05-20 12:28:25 +0300

    sxc

--- Lecture7/Generics/Program.cs
+++ Lecture7/Generics/Program.cs
@@ -18,6 +18,13 @@ namespace Generics
       
         static void Main(string[] args)
         {
+            Queue<int> intQueue = new Queue<int>();
+            intQueue.Enqueue(1);
+            intQueue.Enqueue(2);
+            intQueue.Enqueue(3);
+            int res = intQueue.Dequeue();
+            Console.WriteLine(res);
+
             GenList<int> intList = new GenList<int>();
             for (int i = 1; i <= 10; i++)
                 intList.PushBack(i);
@@ -30,7 +37,18 @@ namespace Generics
             intList[1] = 200;
             intList.Disp();
 
-           
+           //генеричен метод (вграден в Main)
+           void swap<T>(ref T x, ref T y)
+            {
+                T buf = x;
+                x = y;
+                y = buf;
+            }
+
+            int a = 5, b = 10;
+            swap(ref a, ref b);
+
+            Console.WriteLine($"a = {a}, b = {b}");
         }
     }
 }
